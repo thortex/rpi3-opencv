8b65ccfe610e88c95aecb671593f6f6434bee3f9
Thor Watanabe <thormac@gmail.com>
2018-07-17 04:19:04 +0900

    Updated install scripts.

--- release/install_opencv.sh
+++ release/install_opencv.sh
@@ -2,10 +2,17 @@
 U=https://github.com/thortex/rpi3-opencv/releases/download/
 R=v3.4.2-opencl
 V=3.4.2-20180716
+F=opencv_${V}_armhf.deb
 
-F=${U}${R}/opencv_${V}_armhf.deb
+echo -n '' > $F
+for x in a b c d e f g; do
+    X=${U}${R}/opencv_${V}_armhf.xa$x
+    wget -c $X
+    cat $X >> $F
+done
 
-wget -c $F
-
-sudo dpkg -i $F
-sudo ldconfig
+sha256sum -c SHA256SUM
+if [ 0 = $? ] ; then
+    sudo dpkg -i $F
+    sudo ldconfig
+fi
